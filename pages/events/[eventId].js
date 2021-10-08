7efd3cbd766041b4e450f1158202f2562d506b3e
Arxgon <iqbalaf@students.undip.ac.id>
2021-10-08 10:40:48 +0700

    thrid section

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,32 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data";
 
 function EventDetailPage() {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<div>
-			<h1>event detail</h1>
-		</div>
-	);
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found!</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,24 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/event/event-list";
+import EventsSearch from "../../components/event/events-serach";
+import { getAllEvents } from "../../dummy-data";
+
 function AllEventsPage() {
-	return (
-		<div>
-			<h1>all events</h1>
-		</div>
-	);
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
 export default AllEventsPage;
